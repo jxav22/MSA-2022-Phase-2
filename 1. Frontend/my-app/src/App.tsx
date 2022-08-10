7473d18e60db751e0941273fde6e3ec6117b5987
jxav22 <94942712+ggeretsae@users.noreply.github.com>
2022-08-10 19:41:01 +1200

    Added CSS formatting

--- 1. Frontend/my-app/src/App.tsx
+++ 1. Frontend/my-app/src/App.tsx
@@ -2,45 +2,56 @@ import axios from "axios";
 import { useState } from 'react';
 import './App.css';
 
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 function App() {
   // Declare a new state variable, which we'll call "word"
   const [word, setWord] = useState("");
   const [wordInfo, setWordInfo] = useState<undefined | any>(undefined);
+  const [definition, setDefinition] = useState("");
 
   const DICTIONARY_BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en";
+
   return (
-    <div>
-      <h1>
-        DICTIONARY DEFINITIONS
-      </h1>
-      
-      <div>
-        <input 
-          type="text" 
-          id="word-to-search" 
-          name="word-to-search" 
-          defaultValue="ENTER A WORD" 
-          onChange={e => setWord(e.target.value)}
-        />
-        <button onClick={search}>
+    <div className="main">
+      <ThemeProvider theme={theme}>
+        <Typography variant="h2">DICTIONARY DEFINITIONS</Typography>
+      </ThemeProvider>
+
+      <TextField
+        id="outlined-search"
+        label="ENTER A WORD"
+        type="search"
+        variant="filled"
+        onChange={e => setWord(e.target.value)}
+      />
+
+      <Button onClick={search} variant="contained">
         Search
-        </button>
-      </div>
+      </Button>
 
-      <p>
-        You have entered {word}
-      </p>
-
-      <div id="dictionary-definition">{wordInfo[0].meanings[0].definitions[0].definition}</div>
+      <ThemeProvider theme={theme}>
+        <Typography variant="h5">{definition}</Typography>
+      </ThemeProvider>
     </div>
   );
 
   function search() {
-    console.log("Started search");
     axios.get(DICTIONARY_BASE_URL + "/" + word).then((res) => {
       setWordInfo(res.data);
+      setDefinition(res.data[0].meanings[0].definitions[0].definition);
     });
-    console.log(wordInfo[0].meanings[0].definitions[0].definition);
   }
 }
 
