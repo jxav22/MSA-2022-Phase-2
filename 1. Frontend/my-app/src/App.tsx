518cbb45b66de5f030d38e8307d0faf32e58b4e2
jxav22 <94942712+ggeretsae@users.noreply.github.com>
2022-08-10 18:20:20 +1200

    Added functionality to search and display word definitions

--- 1. Frontend/my-app/src/App.tsx
+++ 1. Frontend/my-app/src/App.tsx
@@ -1,10 +1,13 @@
+import axios from "axios";
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   // Declare a new state variable, which we'll call "word"
   const [word, setWord] = useState("");
+  const [wordInfo, setWordInfo] = useState<undefined | any>(undefined);
 
+  const DICTIONARY_BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en";
   return (
     <div>
       <h1>
@@ -12,7 +15,13 @@ function App() {
       </h1>
       
       <div>
-        <input type="text" id="pokemon-name" name="pokemon-name" defaultValue="ENTER A WORD" onChange={e => setWord(e.target.value)}/>
+        <input 
+          type="text" 
+          id="word-to-search" 
+          name="word-to-search" 
+          defaultValue="ENTER A WORD" 
+          onChange={e => setWord(e.target.value)}
+        />
         <button onClick={search}>
         Search
         </button>
@@ -21,11 +30,17 @@ function App() {
       <p>
         You have entered {word}
       </p>
+
+      <div id="dictionary-definition">{wordInfo[0].meanings[0].definitions[0].definition}</div>
     </div>
   );
 
-  function search(){
-      alert("Search button has been clicked!");
+  function search() {
+    console.log("Started search");
+    axios.get(DICTIONARY_BASE_URL + "/" + word).then((res) => {
+      setWordInfo(res.data);
+    });
+    console.log(wordInfo[0].meanings[0].definitions[0].definition);
   }
 }
 
